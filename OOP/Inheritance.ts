3124c2f4164a6021141f2d0c7a7ed2e162a2dbb2
farihaa1 <farihaa1403@gmail.com>
2025-05-04 21:54:25 +0600

    inheritance in typescript

---
+++ OOP/Inheritance.ts
@@ -0,0 +1,16 @@
+{
+    class student {
+        name: string;
+        age: number;
+        address: string;
+
+        constructor(name: string, age: number, address: string) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+        getSleep(numOfHours: number) {
+            console.log(`${this.name} will sleep for ${numOfHours}`)
+        }
+    }
+}
